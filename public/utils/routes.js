0495c79668a72714c7634880e8161e3a5b099060
Olaseinde Oluwabiyi <84818811+Ayowalexy@users.noreply.github.com>
2023-05-01 12:34:49 +0100

    updates

--- public/utils/routes.js
+++ public/utils/routes.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'https://online-farm-project.herokuapp.com' //'http://localhost:5000'
+export const baseUrl = 'https://gray-fantastic-armadillo.cyclic.app' //'https://online-farm-project.herokuapp.com' //'http://localhost:5000'
